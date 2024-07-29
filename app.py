26de81bd89e502d67eb6ba1bfc57e60a53d7c758
Ami-thakor <128568711+Ami-thakor@users.noreply.github.com>
2024-07-29 13:15:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from yt_api import video_info
+from insta_api import get_video_info
 
 app = Flask(__name__)
 
@@ -9,10 +10,16 @@ def hello_world():
     return "Hello, World!"
 
 
-@app.route("/info", methods=["GET"])
+@app.route("/yt", methods=["GET"])
 def get_video_data():
     video_url = request.args.get("url")
     data = video_info(video_url)
+    return jsonify(data)
+    
+@app.route("/ig", methods=["GET"])
+def get_video_data():
+    video_url = request.args.get("url")
+    data = get_video_info(video_url)
     return jsonify(data)
 
 
